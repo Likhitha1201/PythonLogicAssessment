f5a45b7a30c4b17305e22981e2124c745bffa116
likhithashylu <likitha.shylu@gmail.com>
2024-09-20 06:29:39 +0530

    [LIKHITHA] add-Applying Logic to calculate the nth Harmonic Number

---
+++ pb5-HarmonicNumber.py
@@ -0,0 +1,31 @@
+""" 
+
+    @Author: Likhitha S
+    @Date: 19-09-2024 9:19
+    @Last Modified by: Likhitha S
+    @Last Modified time: 19-09-2024 21:23
+    @Title: Write a python program to calculate the harmonic number.
+
+"""
+
+# Harmonic Numbers : 3!=3*2*1 , 2!, ....................
+
+"""
+
+    Description: 
+        This function is used to calculate the Harmonic number.
+    Parameters:
+        num->It is used to take the user input as type integer
+    return:
+        It print's the harmonic number.
+        
+"""
+
+num=int(input("Enter the number:"))
+i=1
+while i<num:
+    print(1,"/",i," + ",end=" ")
+    i=i+1
+    
+    if i==num:
+       print("\nThe nth Harmonic number is:",1,'/',num) 
