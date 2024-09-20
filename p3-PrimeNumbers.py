a24a7bba0e1f847a351badaa555e5ae15688eca8
likhithashylu <likitha.shylu@gmail.com>
2024-09-20 13:17:50 +0530

    [LIKHITHA] add-Applying logic to check the given number is prime or not.

--- p3-PrimeNumbers.py
+++ p3-PrimeNumbers.py
@@ -8,18 +8,35 @@
 
 # Prime number : 2, 3, 5, 7, 11.........
 
-"""
-    Description: 
-        This function is used to calculate the prime number.
-    Parameters:
-        num: It is used to take the user input as type integer
-    return:
-        It print's the group of prime number with in given num.
-"""
-num = int(input("Enter the number:"))
-for i in range(2,num):
-    if num%i==0:
-       print('given ',num,' is not a prime number') 
-       break
-else:
-   print(num,' is a prime number')
+def prime(num):
+    """
+     
+        Description: 
+            This function is used to calculate the prime number.
+        Parameters:
+            num: It is used to take the user input as type integer
+        return:
+            It print's the group of prime number with in given num.
+            
+    """
+    
+    
+    for i in range(2,num):
+        if num%i==0:
+            print('given ',num,' is not a prime number') 
+            break
+        else:
+            print(num,' is a prime number')
+            
+            
+def main():
+    """
+    num is used to take an input from the user.
+    """
+    
+    num = int(input("Enter the number:"))
+    prime(num)
+    
+   
+if __name__=="__main__":
+    main()
