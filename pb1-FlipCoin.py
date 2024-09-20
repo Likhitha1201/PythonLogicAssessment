ad1a951125ad14ec02e4effb3486c8840bbf35bc
likhithashylu <likitha.shylu@gmail.com>
2024-09-20 14:25:38 +0530

    [LIKHITHA] add-Applying logic to check the chances on getting heads and tail

--- pb1-FlipCoin.py
+++ pb1-FlipCoin.py
@@ -10,27 +10,45 @@
 
 # Flip a coin --> 1-Head && 0-Tail
 
-"""
+import random
+
+def chance(side):
+    
+    """
 
             Description: 
                 This function is used to calculate the chances of getting heads and tails.
             Parameters: 
                 side , it helps to decide the no. of head and tail.
             return:
-               It print the random values and gives heads and tails.
+               It print the random values and gives the percentage of getting heads and tails.
 
-"""
-
-import random
-side = random.randint(0,1)
-if side==1:
-    print('Head')
-else:
-    print('Tail')
-if side<0.5:
-    if side==0:
-        print(side,((side+1)*100)//2)
-else:  
-    print(side,(side*100)//2)
+    """
     
-  
+    
+    if side==1:
+        print('Head')
+    else:
+        print('Tail')
+    if side<0.5:
+        if side==0:
+            print(side,((side+1)*100)//2)
+    else:  
+        print(side,(side*100)//2)
+
+def main():
+    
+    """
+    side is used to assign a value to 0 or 1.
+    side 0 indecates Head and 1 indecates tail.
+    """
+    
+    
+    side = random.randint(0,1)
+
+    chance(side)
+        
+        
+if __name__=="__main__":
+    main()
+    
