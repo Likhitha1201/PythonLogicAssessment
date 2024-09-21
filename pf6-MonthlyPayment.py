2fcd411484315e1f8acbdbc379b1b916791f51b3
likhithashylu <likitha.shylu@gmail.com>
2024-09-21 10:35:21 +0530

    [LIKHITHA] add-Applying Logic to calculate monthly payment for the loan taken.

--- pf6-MonthlyPayment.py
+++ pf6-MonthlyPayment.py
@@ -1,8 +1,29 @@
+""" 
+@Author: Likhitha S
+@Date: 21-09-2024 10:45
+@Last Modified by: Likhitha S
+@Last Modified time: 21-09-2024 10:45
+@Title: Write a Python Program with a Util Static Function to calculate monthlyPayment that reads in three command-line arguments P, Y, and R and calculates the monthly payments you would have to make 
+over Y years to pay off a P principal loan amount at R percent interest compounded monthly.
+"""
+
+
 import sys
 
 class LoanCalculator:
     @staticmethod
     def monthly_payment(P, Y, R):
+        """ 
+        Description: 
+            This function is used to calculate the monthly payment for a loan taken.
+        Parameters:
+         pP, Y, R is the parameters is used to bcalculate based on the formula.
+        Return:
+            It returns the monthly_payment as an output. 
+        
+        """
+        
+        
         # Convert annual interest rate R to monthly rate
         r = R / (12 * 100)  
         n = Y * 12          
