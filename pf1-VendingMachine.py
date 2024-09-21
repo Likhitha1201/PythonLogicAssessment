51a2e29bbae9cbb72c4510f11a53748c00db79a0
likhithashylu <likitha.shylu@gmail.com>
2024-09-21 06:32:16 +0530

    [LIKHITHA] add-Applying Logic to calculate the change from a vending machine to return to an user.

--- pf1-VendingMachine.py
+++ pf1-VendingMachine.py
@@ -1,5 +1,31 @@
+""" 
+@Author: Likhitha S
+@Date: 20-09-2024 15:30
+@Last Modified by: Likhitha S
+@Last Modified time: 20-09-2024 15:30
+@Title: Write a Program to calculate the minimum number
+of Notes as well as the Notes to be returned by the Vending Machine as a
+Change
+"""
+
+
 # Function to calculate minimum notes
 def min_notes(change, notes, note_count):
+    """
+    
+    Description: 
+        This function is used to calculate the of the Note to return change
+to get to minimum number of Notes..
+    Parameters:
+        change , notes, note_count= change is an user input that accept the amount in rs.
+        notes it is an empty list.
+        note_count it is used for counting purpose.
+    Return:
+        It returns mini. no. of notes in list format and how many notes required to give a change. 
+    
+    """
+    
+    
     if change == 0:
         return note_count, notes
     
@@ -16,6 +42,13 @@ def min_notes(change, notes, note_count):
 
 # Main program
 def vending_machine(change):
+    """
+    It receive parameter from called fuction.
+    count and result_notes , are used to store the data came from above method
+    After receiving the data it resend it to main method
+     notes_list= is an empty list.
+    """
+    
     notes_list = []
     note_count, notes = min_notes(change, notes_list, 0)
     return note_count, notes
@@ -29,3 +62,5 @@ count, result_notes = vending_machine(change)
 # Output
 print(f"Minimum number of notes needed: {count}")
 print(f"Notes returned: {result_notes}")
+
+
